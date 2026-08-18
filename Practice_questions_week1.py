a94b6f8ac94152b9d833a4ac5bcc965255acf23c
khadar7571 <khadar7571@gmail.com>
2026-08-18 23:15:44 +0530

    Update Practice_questions_week1.py

--- Practice_questions_week1.py
+++ Practice_questions_week1.py
@@ -5,12 +5,16 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score, mean_squared_error
 
+
 # 1) Load a CSV using pandas and print first 10 rows.
+
 
 df = pd.read_csv('Titanic-Dataset.csv')
 print(df.head(10))
 
+
 # 2) Split dataset into train/test using sklearn
+
 
 data = df[['Age', 'Fare']].dropna()
 X = data[['Age']] 
@@ -18,7 +22,9 @@ y = data['Fare']
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42)
 
+
 # 3) Train a Linear Regression model and check accuracy.
+
 
 model = LinearRegression()
 model.fit(X_train, y_train)
@@ -28,7 +34,10 @@ mse = mean_squared_error(y_test, y_pred)
 print(f"R² Score: {r2:.4f}")
 print(f"Mean Squared Error: {mse:.2f}")
 
+
 # 4)Predict ticket fare based on passenger age
+
+
 user_age = float(input("Enter passenger age: "))
 
 predicted_fare = model.predict([[user_age]])
